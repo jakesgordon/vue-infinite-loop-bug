f131877d908eecb5dcbb3e9d765a526df3119479
Jake Gordon <jake@codeincomplete.com>
2020-12-14 19:23:33 -0800

    repro case

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <Foo />
+</template>
+
+<script>
+import Foo from './outer/Foo.vue'
+export default {
+  components: { Foo }
+}
+</script>
---
+++ src/outer/Foo.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>OUTER COMPONENT</h1>
+  <Foo />
+</template>
+<script>
+import Foo from '../inner/Foo.vue'
+export default {
+  components: { Foo }
+}
+</script>
